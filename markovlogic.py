4d5462f4990f02aab5b3fe27e1908032134d65ad
Miranda Ramírez <124285115+mirandanrc@users.noreply.github.com>
2023-02-11 11:59:56 -0600

    Add files via upload

--- markovlogic.py
+++ markovlogic.py
@@ -4,13 +4,13 @@ from std_msgs.msg import String
 import sounddevice as sd
 from scipy.io.wavfile import write
 
-#callback subs
+#callback process
 def markovcallback(msg):
     if(msg.data == "wakeupcall correct" or msg.data == "keep recording"):
-        rospy.loginfo("Valid wakeupcall")
+        #rospy.loginfo("Valid wakeupcall")
         #recording command
         secondscommand = 5  # Duration of recording
-        rospy.loginfo("Command:")
+        rospy.loginfo("Record your command:")
         commandrecording = sd.rec(int(secondscommand * fs), samplerate=fs, channels=2)
         sd.wait()  # Wait until recording is finished
         write('audio/Command.wav', fs, commandrecording)  # Save as WAV file 
@@ -27,26 +27,34 @@ def markovcallback(msg):
 
     if(msg.data == "command recorded"):
         rospy.loginfo("Command: ")
-        
 
+    if(msg.data == "exit call detected"):
+        rospy.loginfo("Exit call detected")
+        
+#callback transcript
 def transcriptcallback(msg):
     rospy.loginfo(msg)
 
+
 #main
-if __name__ == '__main__': 
-    rospy.init_node('markovlogic') 
+if __name__ == '__main__':
+    rospy.init_node('markovlogic')  
     rate= rospy.Rate(2)
     fs = 44100  # Recording sample rate
-
+    
     #subscriber
+    #initsub = rospy.Subscriber("/flaginit", String, initcallback)
     markovsub = rospy.Subscriber("/flags", String, markovcallback)
     transcriptsub = rospy.Subscriber("/whisperradio", String, transcriptcallback)
+    
+
 
     #publisher
     wakeupcallpub = rospy.Publisher("/flags", String, queue_size=50)
     commandpub = rospy.Publisher("/flags", String, queue_size=50)
-    #exitpub = rospy.Publisher("/flag", String, queue_size=50)
-  
+    #startpub = rospy.Publisher("/flaginit", String, queue_size=50)  
+    
+
     #recording wakeupcall
     secondswakeupcall = 2  # Duration of recording
     rospy.loginfo("Wakeup call:")
@@ -59,8 +67,7 @@ if __name__ == '__main__':
     if (flag_wakeupcall == True):
         flagsmarkov = String()
         flagsmarkov.data = "wakeupcall recorded"
-        wakeupcallpub.publish(flagsmarkov)   
-
+        wakeupcallpub.publish(flagsmarkov) 
 
     while not rospy.is_shutdown():
         rate.sleep()
--- whisperlogic.py
+++ whisperlogic.py
@@ -3,32 +3,37 @@ import whisper
 from std_msgs.msg import String
 
 #callback subs
-
 def whispercallback(msg):
-    validwakeupcalls = [" Markovito", " markovito", " Marcovito", " marcovito", " Marko Vito", " Marko vito", " marko Vito", " marko vito", " Marco vito", " Marco Vito", " marco vito", " marco Vito"]
+    validwakeupcalls = [" Markovito", " Markovito.", " markovito", " markovito.", " Marcovito", " Marcovito.", " marcovito", " marcovito.", " Marko Vito", " Marko Vito.", " Marko vito", " Marko vito.", " marko Vito", " marko Vito.", " marko vito", " marko vito.", " Marco Vito", " Marco Vito.", " Marco vito", " Marco vito.", " marco Vito", " marco Vito.", " marco vito", " marco vito."]
+    exitcalls = ["Exit", "exit", "Exit.", "exit."]
+    model = whisper.load_model("base")
 
     if (msg.data == "wakeupcall recorded"):
         wakedata = True
 
         while (wakedata == True):
             rospy.loginfo("Flag Status: Wakeupcall is ready")
-            model = whisper.load_model("small")
             wakeresult = model.transcribe("audio/Markovito.wav")
             wakeupcall = String()
             wakeupcall.data = wakeresult["text"]
+            #wakeupcallcheck = String()
+            #wakeupcallcheck.data = wakeresult["text"].split()
             rospy.loginfo(wakeupcall)
 
             if wakeupcall.data in validwakeupcalls:
                 rospy.loginfo("Valid wakeupcall")
                 flagswhisper = String()
                 flagswhisper.data = "wakeupcall correct"
-                #rospy.loginfo(flagswhisper[0])
                 flagsmarkov.publish(flagswhisper)
                 rospy.loginfo("Valid wakeupcall flag sent")
                 wakedata = False
             else:
                 rospy.loginfo("Invalid wakeupcall")
                 flagsmarkov.publish("invalid wakeupcall")
+                #rospy.sleep(1)
+                #flagstart = String()
+                #flagstart.data = "init flag"
+                #initpub.publish(flagstart)
                 wakedata = False
 
     if (msg.data == "command is ready"):
@@ -36,12 +41,12 @@ def whispercallback(msg):
                         
         while (commanddata == True):                
             rospy.loginfo("Flag Status: Command is ready")
-            model = whisper.load_model("small")
             comresult = model.transcribe("audio/Command.wav")
             command = String()
             command.data = comresult["text"]
             exitcheck = String()
             exitcheck.data = comresult["text"].split()
+            print(exitcheck)
             rospy.loginfo(command)
             #publish transcript is ready
             flagswhisper = String()
@@ -52,26 +57,40 @@ def whispercallback(msg):
             rospy.sleep(2)
             whisperpub.publish(command)
             rospy.loginfo("Command transcript sent")
-            commanddata = False
-                
 
+            rospy.sleep(2)
+
+
+            if exitcheck.data[-1] in exitcalls:
+                rospy.loginfo("Exit call detected")
+                exitflag = String()
+                exitflag.data = "exit call detected"
+                flagsmarkov.publish(exitflag)
+                commanddata = False
+            else:
+                rospy.loginfo("Exit call  not detected, recording will continue")
+                keeprecording = String()
+                keeprecording.data = "keep recording"
+                flagsmarkov.publish(keeprecording)
+                commanddata = False
         
 #main
 if __name__ == '__main__': 
     rospy.init_node('whisperlogic')
-    rate= rospy.Rate(2) 
+    rate = rospy.Rate(2) 
         
     #publisher
     flagsmarkov = rospy.Publisher("/flags", String, queue_size=50)
+    #initpub = rospy.Publisher("/flaginit", String, queue_size=50)
     #flagplease = rospy.Publisher("/whisperradio", String, queue_size=50)
     #flagexit = rospy.Publisher("/whisperradio", String, queue_size=50)
     whisperpub = rospy.Publisher("/whisperradio", String, queue_size=50)
 
     #subscriber
+    #startsub = rospy.Subscriber("/flaginit", String, startcallback)
     whispersub = rospy.Subscriber("/flags", String, whispercallback)
-    #whispersub2 = rospy.Subscriber("/flags", String, whispercallback2)
     #whispersub3 = rospy.Subscriber("/flags", String, whispercallback3)
-         
+    
     while not rospy.is_shutdown():
         rate.sleep()
      
